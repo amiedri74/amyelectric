392a52ff6363779507677c3a1692300220d3126e
Amram Edry <ami.edri@gmail.com>
2026-09-07 09:37:48 -0700

    Add XML sitemap endpoint

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,25 @@
+const serviceSlugs = [
+  'ev-charger-installation',
+  'tesla-wall-connector',
+  'panel-upgrades',
+  'electrical-repair',
+  'recessed-lighting',
+  'commercial-electrical',
+  'new-construction-wiring',
+  'whole-home-rewiring',
+  'dedicated-circuits'
+]
+
+export async function getServerSideProps({ res }) {
+  const base = 'https://www.amyelectric.com'
+  const urls = ['/', '/services', ...serviceSlugs.map((slug) => `/services/${slug}`)]
+  const body = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map((url) => `<url><loc>${base}${url}</loc></url>`).join('')}</urlset>`
+  res.setHeader('Content-Type', 'application/xml')
+  res.write(body)
+  res.end()
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  return null
+}
